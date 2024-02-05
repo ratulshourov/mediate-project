8d9ae609066307bbb821f52a734fe314d462ea04
ratul <ratulshourov01@gmail.com>
2024-02-06 00:56:08 +0600

    event date wise user show in the video

--- app/Http/Controllers/VideoChatController.php
+++ app/Http/Controllers/VideoChatController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Console\Scheduling\Event;
 use Illuminate\Http\Request;
 use Pusher\Pusher;
+use Illuminate\Support\Facades\DB;
 class VideoChatController extends Controller
 {
     public function index(Request $request) {
-        
-        $user = $request->user();
-        $others = User::where('id', '!=', $user->id)->pluck('name', 'id');
+        $eventDate = \Carbon\Carbon::now(); // Assuming you have a DateTime instance, you can replace this with your actual date and time
+
+$formattedDateTime = $eventDate->format('Y-m-d');
+// $events  = DB::table('events')->where('event_date', '>=', $formattedDateTime)
+// ->get();
+//         $user = $request->user();
+//         $others = User::where('id', '!=', $user->id)->pluck('name', 'id');
+
+$user = $request->user();
+
+$events = DB::table('events')
+    ->where('event_date', '>=', $formattedDateTime)
+    ->where('created_by', '!=', $user->id)
+    ->pluck('created_by');
+//dd($events);
+$others = User::whereIn('id', $events)->pluck('name', 'id');
+//dd($others);
         return view('video_chat.index')->with([
             'user' => collect($request->user()->only(['id', 'name'])),
             'others' => $others
