7e4fc8c495b908d06904d2b9594a3224017ab6a5
ratul <ratulshourov01@gmail.com>
2024-02-07 00:11:10 +0600

    stripe package added but not creditial not working

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,34 @@ Route::get('/front', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/charge', function () {
+    return view('charge');
+});
+
+Route::post('/charge', function (Request $request) {
+    // Set your Stripe API key.
+    \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+    // Get the payment amount and email address from the form.
+    $amount = $request->input('amount') * 100;
+    $email = $request->input('email');
+
+    // Create a new Stripe customer.
+    $customer = \Stripe\Customer::create([
+        'email' => $email,
+        'source' => $request->input('stripeToken'),
+    ]);
+    
+    // Create a new Stripe charge.
+    $charge = \Stripe\Charge::create([
+        'customer' => $customer->id,
+        'amount' => $amount,
+        'currency' => 'usd',
+    ]);
+
+    // Display a success message to the user.
+    return 'Payment successful!';
+});
 Route::group(['middleware' => 'auth'], function(){
     Route::get('video_chat', [VideoChatController::class,'index']);
     Route::post('auth/video_chat',[VideoChatController::class,'auth']);
